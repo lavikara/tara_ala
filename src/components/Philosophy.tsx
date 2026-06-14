f187e057618de27ff14b76f0ed843b3bd9497d69
lavikara <karatemitayo@gmail.com>
2026-06-14 11:21:08 +0100

    implement home page philosophy section

---
+++ src/components/Philosophy.tsx
@@ -0,0 +1,11 @@
+export default function Philosophy() {
+  return (
+    <section className="phil">
+      <blockquote className="ph-q rv">
+        &ldquo;The considered dresser doesn&apos;t follow fashion &mdash; she{" "}
+        <em>converses </em> with it.&rdquo;
+      </blockquote>
+      <p className="ph-a">&mdash; Tara Ala</p>
+    </section>
+  );
+}
