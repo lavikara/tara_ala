37460a464d9986772b80c18548c2e9638c1baa3a
lavikara <karatemitayo@gmail.com>
2026-06-14 09:09:28 +0100

    configure images, and development origins

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["172.24.224.1", "192.168.56.1"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
